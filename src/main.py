9343744f9cc56ea3eb11820adeb9bfad789e91e7
Dai Kiddle <dak55@uclive.ac.nz>
2020-08-10 11:29:29 +1200

    initializing main function

---
+++ src/main.py
@@ -0,0 +1,9 @@
+'''runs program'''
+from config import *
+from query import *
+from calculate_ede import *
+
+def main(state):
+    '''main'''
+    db, context = cfg_init(state)
+    origxdest = query_points(db, context)
