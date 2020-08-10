11bcd61e118c29aac5a6e565a77f7bb8063584a4
Dai Kiddle <dak55@uclive.ac.nz>
2020-08-10 15:01:30 +1200

    Updating nearest service so it is in a working condition

--- src/main.py
+++ src/main.py
@@ -2,9 +2,10 @@
 from config import *
 from query import *
 from calculate_ede import *
+from nearest_service import *
 
 def main(state):
     '''main'''
     db, context = cfg_init(state)
     origxdest = query_points(db, context)
-    
+    nearest_service = find_nearest_service(origxdest)
--- src/nearest_service.py
+++ src/nearest_service.py
@@ -1,9 +1,15 @@
 from config import *
 
-def find_nearest_service():
+def main():
+    state = 'ch'
+    db, context = cfg_init(state)
+    origxdest = pd.read_sql("SELECT * FROM baseline_distance", db['con'])
+    find_nearest_service(origxdest)
+
+def find_nearest_service(distances):
     '''takes a distance matrix and returns a matrix of 1 origin per service
     paired with the nearest service to that origin'''
-    db, context = cfg_init(state)
+    db, context = cfg_init('ch')
     con = db['con']
     cursor = con.cursor()
 
@@ -12,10 +18,6 @@ def find_nearest_service():
     # init the dataframe
     df = pd.DataFrame(columns = ['id_orig','distance','service'])
 
-    # get the distance matrix
-    distances = pd.read_sql('SELECT * FROM baseline_distance', db['con'])
-    #making the id_dest column the index
-    distances = distances.set_index('id_dest')
     #converts distance column from string to float
     distances.distance = pd.to_numeric(distances.distance)
     # block ids
@@ -32,20 +34,14 @@ def find_nearest_service():
     #sorts by id_orig
     df.sort_values(by=['id_orig', 'service'], inplace=True)
     # add df to sql, if it exists it will be replaced
-    code.interact(local=locals())
-    df.to_sql('nearest_dist', con=db['engine'], if_exists='replace', index=False)
-    # add index
-    cursor.execute('CREATE INDEX on nearest_dist (time_stamp);')
-    # commit
-    con.commit()
-    con.close()
+    return df
 
 def nearest_distance(distances, ids_open):
     '''
     Calculates the nearest distance matrix for a particular service in a particular time_step
     '''
-    # subset the distance matrix on dest_id
-    dists_sub = distances.loc[ids_open]
+
+    dists_sub = distances.loc[distances['id_dest'].isin(ids_open['id'])]
     # get the minimum distance
     df_min = dists_sub.groupby('id_orig')['distance'].min()
     # prepare df to append: This makes distance the name of the column not the series
@@ -54,5 +50,4 @@ def nearest_distance(distances, ids_open):
     df_min.reset_index(inplace=True)
     return df_min
 
-
-find_nearest_service()
+main()
