7facdf7c96eaf1131b92dffef93262a01c627ba0
Dai Kiddle <dak55@uclive.ac.nz>
2020-08-13 12:24:21 +1200

    typo

--- src/calculate_ede.py
+++ src/calculate_ede.py
@@ -43,7 +43,6 @@ def kp_ede(demo, nearest_service, context):
 
 def kolm_pollak_ede(a, beta = None, kappa = None, weights = None):
     '''returns the Kolm-Pollak EDE'''
-    print(weights)
     a = np.asanyarray(a)
     if kappa is None:
         if beta is None:
