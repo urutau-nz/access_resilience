04376b9fcdd7719bdb8a585c72bd32e668da76a9
MitchellAnderson112 <man112@uclive.ac.nz>
2020-10-21 09:00:47 +1300

    1000 ch multi

--- src/program.py
+++ src/program.py
@@ -23,7 +23,7 @@ def main_function(state):
     baseline_distance = query_points(dest_ids, db, context)
     baseline_nearest = find_nearest_service(baseline_distance, dest_ids, db, context)
     #intilise hazard, df to save and find dests exposed at each fragility level
-    hazard_type = 'hurricane'#'hurricane'#'multi'#'liquefaction'
+    hazard_type = 'multi'#'hurricane'#'multi'#'liquefaction'
     exposure_df = initialise_hazard.open_hazard(hazard_type, db, context)
     # get gpd df of roads with inundation depths and damage bands
     exposed_roads = drop_roads.open_hazard(hazard_type, db, context)
@@ -31,7 +31,7 @@ def main_function(state):
     #open demographic data
     demo = demographic_data(baseline_nearest, db, context)
     #number of iterations for the simulation
-    nsim = 3
+    nsim = 1000
     for i in tqdm(range(nsim)):
         #close destinations
         dest_ids = dests_to_drop(exposure_df, hazard_type, db, context)
@@ -51,32 +51,30 @@ def main_function(state):
     if save_matrix == True:
         write_to_postgres(nearest_matrix, db, 'nearest_distance', indices=False)
         print('Saved nearest distance matrix to SQL')
+    else:
+        plotting(baseline_nearest, nearest_matrix, demo, db, context, hazard_type, nsim)
+        #having a gander at some results
+        #update something horribly wrong is going on here
+        ede_matrix = pd.DataFrame()
+        for j in nearest_matrix.sim_num.unique():
+            subset = nearest_matrix.loc[nearest_matrix['sim_num'] == j]
+            ede_df = kp_ede_main(demo, subset, context)
+            ede_df['sim_num'] = j
+            ede_matrix = pd.concat([ede_matrix, ede_df], ignore_index=True)
 
-    plotting(baseline_nearest, nearest_matrix, demo, db, context, hazard_type, nsim)
-
-
-    #having a gander at some results
-    #update something horribly wrong is going on here
-    ede_matrix = pd.DataFrame()
-    for j in nearest_matrix.sim_num.unique():
-        subset = nearest_matrix.loc[nearest_matrix['sim_num'] == j]
-        ede_df = kp_ede_main(demo, subset, context)
-        ede_df['sim_num'] = j
-        ede_matrix = pd.concat([ede_matrix, ede_df], ignore_index=True)
-
-    sup = ede_matrix.loc[(ede_matrix['population_group'] == 'total') & (ede_matrix['dest_type'] == 'medical_clinic')]
-    mean = sup.ede.mean()
-    print('mean medical_centers = {}\n'.format(mean))
-    tenth = sup.ede.quantile(0.1)
-    print('10th percentile = {}\n'.format(tenth))
-    ninety = sup.ede.quantile(0.9)
-    print('90th percentile = {}\n'.format(ninety))
+        sup = ede_matrix.loc[(ede_matrix['population_group'] == 'total') & (ede_matrix['dest_type'] == 'medical_clinic')]
+        mean = sup.ede.mean()
+        print('mean medical_centers = {}\n'.format(mean))
+        tenth = sup.ede.quantile(0.1)
+        print('10th percentile = {}\n'.format(tenth))
+        ninety = sup.ede.quantile(0.9)
+        print('90th percentile = {}\n'.format(ninety))
 
 
 
 
 #if __name__ == "__main__":
-state = 'tx'#input('State: ')
+state = 'ch'#input('State: ')
 main_function(state)
 
 #calculate ede's
