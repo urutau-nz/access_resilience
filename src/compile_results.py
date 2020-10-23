a0349f6bfc69511d2e9459a692116e3b76f6f71e
MitchellAnderson112 <man112@uclive.ac.nz>
2020-10-24 10:14:51 +1300

    wa liq

--- src/compile_results.py
+++ src/compile_results.py
@@ -3,8 +3,8 @@ from get_demo import *
 from nearest_service import *
 from query import *
 
-state = 'ch' #('ch', 'wa', 'tx')
-hazard = 'tsunami'  #('liquefaction', 'tsunami', 'hurricane', 'multi')
+state = 'wa' #('ch', 'wa', 'tx')
+hazard = 'liquefaction'  #('liquefaction', 'tsunami', 'hurricane', 'multi')
 print('Compiling results for {} under a {} hazard scenario'.format(state, hazard))
 
 def main():
