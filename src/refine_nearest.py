68f3d67fb9e79e407347859e90aed416bf64dfc6
Dai Kiddle <dak55@uclive.ac.nz>
2020-10-23 18:33:02 +1300

    function to refine distance matrix into consistent format for plots

---
+++ src/refine_nearest.py
@@ -0,0 +1,70 @@
+from config import *
+from get_demo import *
+from nearest_service import *
+from query import *
+
+def refine_nearest_distance(baseline_nearest, demo, db, context):
+    '''refines the nearest distance matrix so it has a consistent format between all cities.
+    allows for plotting'''
+    #to work with a smaller matrix
+    df.sim_num = pd.to_numeric(df.sim_num)
+    df = df.loc[df['sim_num'] < 10]
+    #intialise new df
+    refined_df = pd.DataFrame(columns=[
+    'id_orig', 'base_supermarket', 'base_medical_clinic', 'base_primary_school', 'mean_supermarket',
+    'mean_medical_clinic', 'mean_primary_school', '95_supermarket', '95_medical_clinic', '95_primary_school',
+    '5_supermarket', '5_medical_clinic', '5_primary_school'
+    ])
+    #replace NaN
+    df.fillna('isolated', inplace=True)
+    #determine percentage of time that block is isolated from services
+    id_orig = df.id_orig.unique()
+    for i in tqdm(range(len(id_orig))):
+        #get subset of nearest matrix that includes id
+        id = id_orig[i]
+        df_subset = df.loc[df['id_orig'] == id]
+        #create tempoary dictionary to append to the df
+        dict = {'id_orig': id}
+        #determine level of isolated from each service type
+        for service in context['services']:
+            #subset df of orig ids to include only a particular service
+            df_service = df_subset.loc[df_subset['dest_type'] == service]
+            #find proportion of simulations that service is isolated
+            df_iso = df_service.loc[df_service['distance'] == 'isolated']
+            chance_iso = len(df_iso)/len(df_service)
+            #determine if service is cut off
+            if chance_iso >= 0.9:
+                isolated = 'True'
+            else:
+                isolated = 'False'
+                #find rows that have access
+                df_access = df_service.loc[df_service['distance'] != 'isolated']
+                #add data to dict
+                dict['mean_{}'.format(service)] = df_access.distance.mean()
+                dict['95_{}'.format(service)] = df_access.distance.quantile(0.95)
+                dict['5_{}'.format(service)] = df_access.distance.quantile(0.05)
+            #add to tempoary dictionary to store results
+            dict['isolated_{}'.format(service)] = isolated
+            baseline = baseline_nearest.loc[(baseline_nearest['id_orig'] == id) & (baseline_nearest['dest_type'] == service)].distance
+            dict['base_{}'.format(service)] = baseline.iloc[0]
+
+        #append to df, note that there are faster ways to do this https://stackoverflow.com/questions/10715965/add-one-row-to-pandas-dataframe
+        refined_df = refined_df.append(dict, ignore_index=True)
+
+    #add demo data to the df
+    #to get consist values with the refined_df
+    demo.id_orig = demo.id_orig.astype(int)
+    demo.id_orig = demo.id_orig.astype(str)
+    #ensures values are sorted correctly
+    demo.sort_values(by=['id_orig'], inplace=True, ignore_index=True)
+    refined_df.sort_values(by=['id_orig'], inplace=True, ignore_index=True)
+    #add to df
+    refined_df['total_pop'] = demo['total']
+    refined_df['white'] = demo['white']
+    refined_df['indigenous'] = demo['indigenous']
+    refined_df['asian'] = demo['asian']
+    refined_df['polynesian'] = demo['polynesian']
+    refined_df['latino'] = demo['latino']
+    refined_df['african_american'] = demo['african_american']
+
+    return refined_df
